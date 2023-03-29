e06725aa93add67e1b2b2b06c0d1f9f45e70f452
A-Pogam <alicepogam59@hotmail.com>
2023-03-29 12:09:07 +0200

    Improvement clarity login and created html page for login

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,8 +2,8 @@ const submit = document.getElementById("submit");
 const errorInformation = document.getElementById("errorInformation");
 // Selectionne les éléments du bouton du formulaire et de l'élément d'affichage des erreurs dans le document HTML en utilisant les identifiants correspondants //
 
-submit.addEventListener("click", (e) => {
-    e.preventDefault();
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
     const email = document.getElementById("emailUser").value;
     const password = document.getElementById("password").value;
     // Ecouteur d'événements pour le clic sur le bouton de soumission. Lorsque l'événement se produit, la fonction fléchée anonyme est exécutée. Elle empêche également l'action par défaut de soumission du formulaire en appelant la méthode preventDefault() sur l'événement passé en argument. écupère les valeurs des champs d'e-mail et de mot de passe du formulaire en utilisant la méthode getElementById() //
